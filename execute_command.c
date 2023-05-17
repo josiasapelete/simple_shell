e7f842b2c06292125c7f96ac688db3ff19d7323f
liams2king <nguewa237@icloud.com>
2023-05-17 12:37:50 -0700

    execute_command.c

---
+++ execute_command.c
@@ -0,0 +1,50 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+#include "shell.h"
+
+/**
+ * execute_command - Execute a command
+ * @command: The command to execute
+ * @args: Arguments for the command
+ *
+ * Description: This function executes the specified command by forking
+ * a new process and using the execve() system call.
+ *
+ * Return: On success, returns 0. On failure, returns -1.
+ */
+int execute_command(char *command, char **args)
+{
+	pid_t pid;
+	int status;
+
+
+	pid = fork();
+	if (pid == 0)
+	{
+
+		if (execve(args[0], args, NULL) == -1)
+		{
+			perror("Error executing command");
+			exit(EXIT_FAILURE);
+		}
+	}
+	else if (pid < 0)
+	{
+		perror("forking error");
+		exit(EXIT_FAILURE);
+	}
+	else
+	{
+
+		do {
+			waitpid(pid, &status, WUNTRACED);
+		} while (!WIFEXITED(status) && !WIFSIGNALED(status));
+	}
+
+
+	return (1);
+}
