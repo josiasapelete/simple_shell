66d29511869928570ecd60c53324941848d389e0
liams2king <nguewa237@icloud.com>
2023-05-17 11:44:05 -0700

    shell.h

---
+++ shell.h
@@ -0,0 +1,18 @@
+#ifndef _SHELL_H_
+#define _SHELL_H_
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <string.h>
+
+
+void prompt(void);
+char **split_input(char *input);
+int execute_command(char **args);
+void free_args(char **args);
+
+#endif
+
