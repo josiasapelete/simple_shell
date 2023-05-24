ab62e1c840c09e72fbdd8182eb5dbb09fbc01315
liams2king <nguewa237@icloud.com>
2023-05-24 13:14:31 -0700

    update

--- free_args.c
+++ free_args.c
@@ -12,16 +12,9 @@
  */
 void free_args(char **args)
 {
-	/*int i;*/
 
 	if (args == NULL)
 		return;
-
-	/*for (i = 0; args[i] != NULL; i++)
-	{
-		free(args[i]);
-		args[i] = NULL;
-	}*/
 
 	free(args);
 }
